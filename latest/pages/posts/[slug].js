7138f26d4fb0ce3b52d280884e8f04deaad9f58d
David Konsumer <konsumer@jetboystudio.com>
2023-01-10 18:11:43 -0800

    fix routing

--- latest/pages/posts/[slug].js
+++ latest/pages/posts/[slug].js
@@ -1,52 +1,98 @@
-import { Layout } from "../../components/Layout";
-import { useTina } from "tinacms/dist/react";
-import { client } from "../../.tina/__generated__/client";
+import { Layout } from '../../components/Layout'
+import { useTina } from 'tinacms/dist/react'
+import { client } from '../../.tina/__generated__/client'
 
-export default function Home(props) {
+export default function Home (props) {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
   const { data } = useTina({
     query: props.query,
     variables: props.variables,
-    data: props.data,
-  });
+    data: props.data
+  })
 
   return (
     <Layout>
       <code>
         <pre
           style={{
-            backgroundColor: "lightgray",
+            backgroundColor: 'lightgray'
           }}
         >
           {JSON.stringify(data.post, null, 2)}
         </pre>
       </code>
     </Layout>
-  );
+  )
 }
 
+// export const getStaticPaths = async () => {
+//   const { data } = await client.queries.postConnection();
+//   const paths = data.postConnection.edges.map((x) => {
+//     return { params: { slug: x.node._sys.filename } };
+//   });
+//
+//   return {
+//     paths,
+//     fallback: "blocking",
+//   };
+// };
+//
+// export const getStaticProps = async (ctx) => {
+//   const { data, query, variables } = await client.queries.post({
+//     relativePath: ctx.params.slug + ".md",
+//   });
+//
+//   return {
+//     props: {
+//       data,
+//       query,
+//       variables,
+//     },
+//   };
+// };
+
+// query to just get all the slugs
+export const ALL_BLOG_SLUGS = `
+query ALL_BLOG_SLUGS {
+  postConnection(last: -1 ) {
+    edges {
+      node {
+        slug
+      }
+    }
+  }
+}
+`
+
+// generate a list of all possible slug-values
 export const getStaticPaths = async () => {
-  const { data } = await client.queries.postConnection();
+  const { data } = await client.request({ query: ALL_BLOG_SLUGS })
   const paths = data.postConnection.edges.map((x) => {
-    return { params: { slug: x.node._sys.filename } };
-  });
+    return { params: { slug: x.node.slug } }
+  })
 
   return {
     paths,
-    fallback: "blocking",
-  };
-};
+    fallback: false
+  }
+}
 
+// search for post with slug, and load that
 export const getStaticProps = async (ctx) => {
-  const { data, query, variables } = await client.queries.post({
-    relativePath: ctx.params.slug + ".md",
-  });
+  const r = await client.queries.postConnection({ filter: { slug: { eq: ctx.params.slug } } })
+  if (!r?.data?.postConnection?.edges?.length) {
+    return {
+      notFound: true
+    }
+  }
+
+  const { data, query, variables } = await client.queries.post({ relativePath: (r.data.postConnection.edges).pop()?.node?._sys?.relativePath })
 
   return {
     props: {
       data,
       query,
-      variables,
-    },
-  };
-};
+      variables
+    }
+  }
+}
--- latest/pages/posts/index.js
+++ latest/pages/posts/index.js
@@ -1,41 +1,41 @@
-import { Layout } from "../../components/Layout";
-import Link from "next/link";
-import { useTina } from "tinacms/dist/react";
-import { client } from "../../.tina/__generated__/client";
+import { Layout } from '../../components/Layout'
+import Link from 'next/link'
+import { useTina } from 'tinacms/dist/react'
+import { client } from '../../.tina/__generated__/client'
 
-export default function PostList(props) {
+export default function PostList (props) {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
   const { data } = useTina({
     query: props.query,
     variables: props.variables,
-    data: props.data,
-  });
-  const postsList = data.postConnection.edges;
+    data: props.data
+  })
+  const postsList = data.postConnection.edges
   return (
     <Layout>
       <h1>Posts</h1>
       <div>
         {postsList.map((post) => (
           <div key={post.node.id}>
-            <Link href={`/posts/${post.node._sys.filename}`}>
-              <a>{post.node._sys.filename}</a>
+            <Link href={`/posts/${post.node.slug}`}>
+              <a>{post.node.title}</a>
             </Link>
           </div>
         ))}
       </div>
     </Layout>
-  );
+  )
 }
 
 export const getStaticProps = async () => {
-  const { data, query, variables } = await client.queries.postConnection();
+  const { data, query, variables } = await client.queries.postConnection()
 
   return {
     props: {
       data,
       query,
-      variables,
-      //myOtherProp: 'some-other-data',
-    },
-  };
-};
+      variables
+      // myOtherProp: 'some-other-data',
+    }
+  }
+}
